0c57446422a037d395003fb78c2a8116c2c94b6f
Wender Dantas Nascimento <wender_dantas@hotmail.com>
2022-09-30 15:10:00 -0300

    calendar

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+d
+4app/src/main/java/br/com/estudos/utils/Extensions.kt,2\6\26788cb9bf80a12b5c5fb35a45488afd71c1ca06
---
+++ app/src/main/java/br/com/estudos/ui/Types.kt
@@ -0,0 +1,19 @@
+package br.com.estudos.ui
+
+import android.view.View
+import br.com.estudos.model.CalendarDay
+import br.com.estudos.model.CalendarMonth
+
+open class ViewContainer(val view: View)
+
+interface DayBinder<T : ViewContainer> {
+    fun create(view: View): T
+    fun bind(container: T, day: CalendarDay)
+}
+
+interface MonthHeaderFooterBinder<T : ViewContainer> {
+    fun create(view: View): T
+    fun bind(container: T, month: CalendarMonth)
+}
+
+typealias MonthScrollListener = (CalendarMonth) -> Unit
